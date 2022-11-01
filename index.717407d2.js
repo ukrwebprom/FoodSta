226ec5bab4bddc674bf710f32b50cda0a8ce68b1
ukrwebprom <ukrwebprom@gmail.com>
2022-11-01 19:06:37 +0000

    Deploying to gh-pages from @ ukrwebprom/FoodSta@7385c43b31f66cce5144ffd8780d51a39da68877 🚀

---
+++ index.717407d2.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.717407d2.js.map
